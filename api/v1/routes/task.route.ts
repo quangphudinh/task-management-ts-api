0a4d22067b667be4c6cc44ebe383e7e7ef4df5f4
John Doe <johndoe@email.com>
2024-10-17 22:47:05 +0700

    tạo controller

--- api/v1/routes/task.route.ts
+++ api/v1/routes/task.route.ts
@@ -1,24 +1,11 @@
-import { Router ,Request, Response } from 'express';
-import {Task} from '../../../models/task.model';
+import { Router } from 'express';
 const router : Router = Router();
 
+import * as controller from "../controller/task.controller";
 
-router.get("/", async(req : Request, res : Response) => {
-    const tasks = await Task.find({
-        deleted: false
-    });
- 
-    res.json(tasks);
-})
 
-router.get("/detail/:id", async(req : Request, res : Response) => {
-    const id : string = req.params.id;
-    const tasks = await Task.findOne({
-        _id: id,
-        deleted: false
-    });
-   
-    res.json(tasks);
-})
+router.get("/", controller.index);
+
+router.get("/detail/:id", controller.detail);
 
 export const taskRoutes : Router = router;
